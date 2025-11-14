059d12e794f771244c841ab0789c818cfdd13a7d
Pramod Kumar Raghav <praghav@adobe.com>
2025-11-14 05:31:47 +0530

    fix: Update ToastProvider import path in main.jsx
    Changed from ./components/ToastContainer to ./context/ToastContext

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ToastProvider } from './components/ToastContainer';
+import { ToastProvider } from './context/ToastContext';
 import App from './App';
 import './index.css';
 
